f395b7f980bb63ec6c4c6e14e768daa882cdffff
Eduardo Rabelo <oieduardorabelo@gmail.com>
2023-03-15 21:25:14 +1300

    feat: add unit tests

--- test/cdk-eventbridge-scheduler-last-modified.test.ts
+++ test/cdk-eventbridge-scheduler-last-modified.test.ts
@@ -1,17 +1,61 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkEventbridgeSchedulerLastModified from '../lib/cdk-eventbridge-scheduler-last-modified-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-eventbridge-scheduler-last-modified-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkEventbridgeSchedulerLastModified.CdkEventbridgeSchedulerLastModifiedStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+process.env.TARGET_URL = "my-uni-test-url";
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+import { SchedulerLastModifiedStack } from "../lib/cdk-eventbridge-scheduler-last-modified-stack";
+
+test("creates a rule that runs on a schedule every Friday at 07:00 am", () => {
+  const app = new cdk.App();
+  const stack = new SchedulerLastModifiedStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::Events::Rule", {
+    ScheduleExpression: "cron(0 7 ? * FRI *)",
+    State: "ENABLED",
+  });
+});
+
+test("creates a rule with lambda function as target", () => {
+  const app = new cdk.App();
+  const stack = new SchedulerLastModifiedStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::Events::Rule", {
+    Targets: [
+      {
+        Arn: Match.objectLike({
+          "Fn::GetAtt": [
+            Match.stringLikeRegexp("FetchLastModifiedLambda"),
+            "Arn",
+          ],
+        }),
+        Id: Match.stringLikeRegexp("Target"),
+      },
+    ],
+  });
+});
+
+test("creates last modified table in DynamoDB", () => {
+  const app = new cdk.App();
+  const stack = new SchedulerLastModifiedStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::DynamoDB::Table", {
+    KeySchema: [{ AttributeName: "url", KeyType: "HASH" }],
+    AttributeDefinitions: [{ AttributeName: "url", AttributeType: "S" }],
+  });
+});
+
+test("creates correct permissions for lambda to read/write to DynamoDB", () => {
+  const app = new cdk.App();
+  const stack = new SchedulerLastModifiedStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::IAM::Policy", {
+    PolicyName: Match.stringLikeRegexp("FetchLastModifiedLambda"),
+    PolicyDocument: {
+      Statement: [
+        {
+          Action: Match.arrayWith(["dynamodb:GetItem", "dynamodb:PutItem"]),
+        },
+      ],
+    },
+  });
 });
